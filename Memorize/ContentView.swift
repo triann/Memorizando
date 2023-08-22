82d19420c6f5a6867d3cc63bde46a60c1a865c40
triann <trian12345@hotmail.com>
2023-08-22 02:11:51 -0400

    Finished RT1 Assignment 1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -2,22 +2,62 @@
 //  ContentView.swift
 //  Memorize
 //
-//  Created by user239481 on 8/21/23.
+//  Created by trian on 8/21/23.
 //
 
 import SwiftUI
 
 struct ContentView: View {
-    let emojis = ["👀","🫀","🦷","🧠","🧠"]
+    let emojis = ["👀","🫀","🦷","🧠","🫁","👅","👣","🖐️"]
+    
+    @State var cardCount: Int = 4
     
     var body: some View {
-        HStack {
-            ForEach(emojis.indices, id: \.self) { index in
+        VStack {
+            ScrollView {
+                cards
+            }
+            Spacer()
+            cardCountAdjusters
+        }
+        .padding()
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self) { index in
                 CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
             }
         }
-        .foregroundColor(.green)
-        .padding()
+        .foregroundColor(.black)
+    }
+    
+    var cardCountAdjusters: some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+                cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 ||	cardCount + offset > emojis.count	)
+    }
+    
+    var cardRemover: some View {
+        return cardCountAdjuster(by: -1, symbol: "minus.circle.fill")
+    }
+    
+    var cardAdder: some View {
+        return cardCountAdjuster(by: +1, symbol: "plus.circle.fill")
     }
 }
 
@@ -28,16 +68,23 @@ struct CardView: View {
     var body: some View {
         ZStack {
             let base = RoundedRectangle(cornerRadius: 12)
-            if Virada {
+            Group {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
                 Text(content).font(.largeTitle)
-            } else {
-                base.fill()
             }
+            .opacity(Virada ? 1 : 0)
+            base.fill().opacity(Virada ? 0 : 1)
+            
         }
         .onTapGesture  {
             Virada.toggle()
         }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
